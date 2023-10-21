05f1c4973cfca4a18063a20a10edb8b8451cbe10
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2023-10-21 23:29:33 +0600

    create delete api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const cors = require('cors');
 const app = express();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
@@ -41,7 +41,13 @@ app.get("/products", async (req, res) => {
     // console.log(result);
   });
 
-
+  app.get("/product/:id", async (req, res) => {
+    const id = req.params.id;
+   
+    const query = { _id: new ObjectId(id) };
+    const result = await productCollection.findOne(query);
+    res.send(result);
+  });
   app.post("/post/product", async (req, res) => {
     const body = req.body;
     console.log(body);
@@ -64,7 +70,13 @@ app.get("/products", async (req, res) => {
   });
 
 
-
+  app.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const query = { _id: new ObjectId(id) };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+  });
 
 
 
